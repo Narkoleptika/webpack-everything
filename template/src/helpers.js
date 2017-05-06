7f3b932735ea875e90cef7a29e5a3d46e60a5275
Joseph <joey@clever.ly>
2017-05-06 13:52:37 -0700

    Fix typo, remove line breaks from generated meta

--- template/src/helpers.js
+++ template/src/helpers.js
@@ -1,5 +1,4 @@
-export const generateHead = ({title = '{{ project }}', description = null, keywords = null})=> `
-${title ? `<title>${title}</title>` : ''}
-${description ? `<meta name="description" contnet="${description}">` : ''}
-${keywords ? `<meta name="keywords" contnet="${keywords}">` : ''}
-`
+export const generateHead = ({title = '{{ project }}', description = null, keywords = null})=> `\
+${title ? `<title>${title}</title>` : ''}\
+${description ? `<meta name="description" content="${description}">` : ''}\
+${keywords ? `<meta name="keywords" content="${keywords}">` : ''}`
